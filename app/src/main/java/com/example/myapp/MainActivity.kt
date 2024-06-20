5311c8d250df175ad68ee9059b0aa45a304485ea
kimuori <jemiruth1@gmail.com>
2024-06-19 21:41:18 -0500

    added bottom sheet with save and cancel button features. padding changes.

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,32 +8,45 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,14 +55,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(
             ComposeView(this).apply{
                 setContent{
+                    val sheetState = rememberModalBottomSheetState()
+                    val scope = rememberCoroutineScope()
+                    var showBottomSheet by remember { mutableStateOf(false)}
+                    var textValueField by remember { mutableStateOf(TextFieldValue("")) }
+
                     Scaffold(
                         topBar = {
                             TopAppBar(title = { Text(text = "Todo List")})},
 
                         floatingActionButton = {
-                            FloatingActionButton(onClick = {}){
+                            FloatingActionButton(
+                                onClick = {showBottomSheet = true}
+                            ){
                                 Icon(
-                                    modifier = Modifier.padding(10.dp, 10.dp),
+                                    modifier = Modifier.padding(12.dp, 12.dp),
                                     painter = painterResource(id = R.drawable.baseline_add_24),
                                     contentDescription = "add"
                                 )
@@ -59,10 +80,58 @@ class MainActivity : AppCompatActivity() {
                         innerPadding ->
                         Column(
                             modifier = Modifier
-                                .padding(innerPadding)
+                                .padding(12.dp, 12.dp)
                                 .background(color = Color.Green)
+                                .verticalScroll(rememberScrollState()) //added scroll in case it is not working
                                 .fillMaxSize()) {
                             ColumnView()
+                        }
+
+                        if (showBottomSheet){
+                            ModalBottomSheet(
+                                onDismissRequest = { showBottomSheet = false },
+                                sheetState = sheetState
+                            ) {
+                                TextField(
+                                    value = textValueField,
+                                    onValueChange = {textValueField = it},
+                                    label = {Text("New Todo")},
+                                    trailingIcon = {Icon(painter = painterResource(id = R.drawable.outline_cancel_24dp), contentDescription = "clear")},
+                                    singleLine = true,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(12.dp, 12.dp)
+                                )
+                                Spacer(modifier = Modifier.size(12.dp))
+                                Button(
+                                    onClick = {
+                                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                            if (!sheetState.isVisible) {
+                                                showBottomSheet = false
+                                            }
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(12.dp, 12.dp)
+                                ) {
+                                    Text("Save")
+                                }
+                                OutlinedButton(
+                                    onClick = {
+                                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                            if (!sheetState.isVisible) {
+                                                showBottomSheet = false
+                                            }
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(12.dp, 12.dp)
+                                ) {
+                                    Text("Cancel")
+                                }
+                            }
                         }
                     }
                 }
@@ -74,7 +143,7 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 private fun ColumnView(){
-    for( i in 0..100 ) {
+    for( i in 0..10 ) {
         TodoList()
     }
 }
@@ -84,7 +153,9 @@ private fun TodoList(){
     Row (
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.background(color = Color.Blue).fillMaxWidth()
+        modifier = Modifier
+            .background(color = Color.Blue)
+            .fillMaxWidth()
     ){
         TodoCheck()
     }
